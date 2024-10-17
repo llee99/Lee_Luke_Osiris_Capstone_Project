cd2a1eefa7f6d80daec5304eb40dc0b6510d1c40
llee99 <121249795+llee99@users.noreply.github.com>
2024-10-17 12:29:37 -0500

    bcrypt

--- osiris_backend/controllers/userController.js
+++ osiris_backend/controllers/userController.js
@@ -1,18 +1,35 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 //@dec Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
         res.status(400);
         throw new Error("All fields are mandatory!");
     }
     const userAvailable = await User.findOne({ email });
     if (userAvailable) {
         res.status(400);
-        throw new Error("All fields are mandatory!")
+        throw new Error("User already exists!");
+    }
+
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password: ", hashedPassword);
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+    console.log(`User created ${user}`);
+    if (user) {
+        res.status(201).json({_id: user.id, email: user.email})
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid");
     }
     res.json({ message: "Register the user" });
 });
